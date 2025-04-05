317000d9f58c78ed4f4bc284075c5b9be5590a73
Nurislom <nurislomx72@gmail.com>
2025-04-05 20:56:27 +0500

    Удалил базу данных из модуля Gateway. #04-05

--- notification-client/src/main/java/org/khasanof/notification/service/RestChannelSender.java
+++ notification-client/src/main/java/org/khasanof/notification/service/RestChannelSender.java
@@ -1,6 +1,5 @@
 package org.khasanof.notification.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.khasanof.notification.NotificationDTO;
 import org.khasanof.notification.service.config.RestChannelConfiguration;
